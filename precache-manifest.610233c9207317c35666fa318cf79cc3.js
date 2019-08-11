7f3a185f4ca0174ed8424198e1dcde06ffa74215
barton-w <bartonw@gmail.com>
2019-08-11 17:09:20 -0600

    Updates

---
+++ precache-manifest.610233c9207317c35666fa318cf79cc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3b86cb6d90cf9bc6b068bbfcbfa0d6",
+    "url": "/sql-sensei/index.html"
+  },
+  {
+    "revision": "e30e2fa70f8c3758c139",
+    "url": "/sql-sensei/static/css/main.e146e021.chunk.css"
+  },
+  {
+    "revision": "8d0bab158c6a51c061e3",
+    "url": "/sql-sensei/static/js/2.21016802.chunk.js"
+  },
+  {
+    "revision": "e30e2fa70f8c3758c139",
+    "url": "/sql-sensei/static/js/main.2aba6d69.chunk.js"
+  },
+  {
+    "revision": "5f97d4451d7bb518e23a",
+    "url": "/sql-sensei/static/js/runtime~main.a0ad6a02.js"
+  }
+]);
